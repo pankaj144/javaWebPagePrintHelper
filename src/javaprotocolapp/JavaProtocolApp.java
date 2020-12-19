9dd5d56cedb0696348441b764605675a4db990d6
priya <priyakuma153@gmail.com>
2020-12-19 15:38:37 +0530

    .reg file in installer

--- src/javaprotocolapp/JavaProtocolApp.java
+++ src/javaprotocolapp/JavaProtocolApp.java
@@ -5,10 +5,12 @@
  */
 package javaprotocolapp;
 
+import java.awt.Desktop;
 import java.awt.FlowLayout;
 import java.awt.print.PrinterJob;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -41,8 +43,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import org.json.JSONObject;
 
-
-
 /**
  *
  * @author hp
@@ -54,32 +54,32 @@ public class JavaProtocolApp {
      */
     private static final String GET_URL = "http://stag-api.futtkr.com//api/user/app_version";
 //    private static String POST_URL = "http://stage-api.futtkr.com/api/shop/getDataToPrint";
-    private static  String POST_URL = "http://demo-api.futtkr.com/api/shop/getDataToPrint";
-    private static final String POST_PARAMS = "1";  
-    
+    private static String POST_URL = "http://demo-api.futtkr.com/api/shop/getDataToPrint";
+    private static final String POST_PARAMS = "1";
+
     public static void main(String[] args) {
-        try{
-             
-            if(args.length > 0){
-               String argument = args[0]; 
-               String replaceProtoclTag = argument.replaceAll("myappprotocol:", "");
-              
-               System.out.print("This is my Protocol java Application");
-               if(replaceProtoclTag.contains("LIVE")){
-                   replaceProtoclTag = replaceProtoclTag.replaceAll("LIVE","");
-                   POST_URL = "http://demo-api.futtkr.com/api/shop/getDataToPrint";
-               }
-               sendPOST(replaceProtoclTag);
-            }else{
-            sendPOST("NOI_S168_UC_51006");
-        }
-          
-        }catch(IOException exp){
+        try {
+            JavaProtocolApp japp = new JavaProtocolApp();
+            if (args.length > 0) {
+                String argument = args[0];
+                String replaceProtoclTag = argument.replaceAll("myappprotocol:", "");
+
+                System.out.print("This is my Protocol java Application");
+                if (replaceProtoclTag.contains("LIVE")) {
+                    replaceProtoclTag = replaceProtoclTag.replaceAll("LIVE", "");
+                    POST_URL = "http://demo-api.futtkr.com/api/shop/getDataToPrint";
+                }
+                sendPOST(replaceProtoclTag);
+            } else {
+                sendPOST("NOI_S168_UC_51006");
+            }
+
+        } catch (IOException exp) {
             exp.printStackTrace();
         }
-          //printData("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPp");
-        
-/*            PrintService mPrinter = null;
+        //printData("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPp");
+
+        /*            PrintService mPrinter = null;
         Boolean bFoundPrinter = false;
 
         PrintService[] printServices = PrinterJob.lookupPrintServices();
@@ -135,16 +135,12 @@ public class JavaProtocolApp {
         } catch (IOException e) {
             e.printStackTrace();
         }
- */           
-            
+         */
         //StringBuilder sb=new StringBuilder();  
-        
 //        for(int i = 0;i < args.length;i++){
 //            sb.append(args[i]);//now original string is changed  
 //        }
-
-        
-       /* JFrame f=new JFrame("Protocol Content");  
+        /* JFrame f=new JFrame("Protocol Content");  
         JButton b=new JButton(sb.toString());  
         b.setBounds(50,50,200,300);  
         f.add(b);  
@@ -152,15 +148,11 @@ public class JavaProtocolApp {
         f.setLayout(null);  
         f.setVisible(true);
         System.out.print("This is my Protocol java Application");
-        */
-        
-        
-       
-           
-
+         */
     }
-    
+
     static class PrintJobWatcher {
+
         // true iff it is safe to close the print job's input stream
         boolean done = false;
 
@@ -170,128 +162,142 @@ public class JavaProtocolApp {
                 public void printJobCanceled(PrintJobEvent pje) {
                     allDone();
                 }
+
                 public void printJobCompleted(PrintJobEvent pje) {
                     allDone();
                 }
+
                 public void printJobFailed(PrintJobEvent pje) {
                     allDone();
                 }
+
                 public void printJobNoMoreEvents(PrintJobEvent pje) {
                     allDone();
                 }
+
                 void allDone() {
                     synchronized (PrintJobWatcher.this) {
                         done = true;
                         PrintJobWatcher.this.notify();
-                         System.out.print(" d!");
-                         System.exit(0);
+                        System.out.print(" d!");
+                        System.exit(0);
                     }
                 }
             });
         }
+
         public synchronized void waitForDone() {
             try {
                 while (!done) {
                     wait();
                 }
-                
+
             } catch (InterruptedException e) {
-                 System.out.print("Error::" +e.toString());
+                System.out.print("Error::" + e.toString());
             }
         }
     }
-    
+
     private static void sendGET() throws IOException {
-		URL obj;
-                obj = new URL(GET_URL);
-		HttpURLConnection con;
-                 con = (HttpURLConnection) obj.openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", "");
-		int responseCode = con.getResponseCode();
-		System.out.println("GET Response Code :: " + responseCode);
-		if (responseCode == HttpURLConnection.HTTP_OK) { // success
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
+        URL obj;
+        obj = new URL(GET_URL);
+        HttpURLConnection con;
+        con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "");
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
 
-			// print result
-			System.out.println(response.toString());
-		} else {
-			System.out.println("GET request not worked");
-		}
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
 
-	}
-    
-    
+    }
+
     private static void sendPOST(String orderId) throws IOException {
-		URL obj = new URL(POST_URL);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", "");
+
+        if (orderId.equalsIgnoreCase("NOI_S168_UC_51006")) {
+            Desktop.getDesktop().open(new File("C:\\Program Files (x86)\\PrinterHelper\\regEdit.reg"));
+        } else {
+
+            URL obj = new URL(POST_URL);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", "");
 //                
-		// For POST only - START
-		con.setDoOutput(true);
-		OutputStream os = con.getOutputStream();
+            // For POST only - START
+            con.setDoOutput(true);
+            OutputStream os = con.getOutputStream();
 //		os.write(POST_PARAMS.getBytes(StandardCharsets.UTF_8));
-                os.write(getPostData(orderId));
-		os.flush();
-		os.close();
-		// For POST only - END
+            os.write(getPostData(orderId));
+            os.flush();
+            os.close();
+            // For POST only - END
 
-		int responseCode = con.getResponseCode();
-		System.out.println("POST Response Code :: " + responseCode);
+            int responseCode = con.getResponseCode();
+            System.out.println("POST Response Code :: " + responseCode);
 
-		if (responseCode == HttpURLConnection.HTTP_OK) { //success
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
+            if (responseCode == HttpURLConnection.HTTP_OK) { //success
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
 
-			// print result
-                        String toPrint="";
-                        
-                        //work around
+                // print result
+                String toPrint = "";
+
+                //work around
 //                        toPrint = cleanData(response.toString());
 //                        System.out.println("CLEAN DATA="+toPrint);
-                        //using json.org library
-                        try{
-                            toPrint = extractData(response.toString());
+                //using json.org library
+                try {
+                    toPrint = extractData(response.toString());
 //                        showDataV2(toPrint);
-                        }catch(Exception exp){
-                            showData("Error on Erxtraction");
-                        }
-                        printData(toPrint);
-			System.out.println(response.toString());
-		} else {
-			System.out.println("POST request not worked");
-		}
-	} 
-    
-    private static class Info{
+                } catch (Exception exp) {
+                    showData("Error on Erxtraction");
+                }
+                printData(toPrint);
+                System.out.println(response.toString());
+            } else {
+                System.out.println("POST request not worked");
+            }
+        }
+    }
+
+    private static class Info {
+
         String shopId;
-        public void setShopId(String shopId){
+
+        public void setShopId(String shopId) {
             this.shopId = shopId;
         }
     }
-    
-     static byte[]  getPostData(String myOrderId)  throws UnsupportedEncodingException{
-        Map<String,Object> params = new LinkedHashMap<>();
+
+    static byte[] getPostData(String myOrderId) throws UnsupportedEncodingException {
+        Map<String, Object> params = new LinkedHashMap<>();
         params.put("orderId", myOrderId);
         StringBuilder postData = new StringBuilder();
-        for (Map.Entry<String,Object> param : params.entrySet()) {
-            if (postData.length() != 0) postData.append('&');
+        for (Map.Entry<String, Object> param : params.entrySet()) {
+            if (postData.length() != 0) {
+                postData.append('&');
+            }
             postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
             postData.append('=');
             postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
@@ -299,55 +305,55 @@ public class JavaProtocolApp {
         byte[] postDataBytes = postData.toString().getBytes("UTF-8");
         return postDataBytes;
     }
-     
-     
-    public static  void printData(String dataToPrint){
+
+    public static void printData(String dataToPrint) {
         PrintService mPrinter = null;
         Boolean bFoundPrinter = false;
 
         PrintService[] printServices = PrinterJob.lookupPrintServices();
         // TODO code application logic here
-        StringBuilder sb=new StringBuilder();  
-        
+        StringBuilder sb = new StringBuilder();
+
 //        for(int i = 0;i < args.length;i++){
 //            sb.append(args[i]);//now original string is changed  
 //        }
-       String attribute = "";
-         for (PrintService printService : printServices) {
-                String sPrinterName = printService.getName();
-                attribute += "--Printer Name---\n";
-                if (sPrinterName.equals("RETSOL RTP-80") || sPrinterName.equals("POS-58")) { //POS-58ew  
-                    mPrinter = printService;
-                    bFoundPrinter = true;
-                }
-                
-                System.out.println("Printer Name::" +" : " + sPrinterName);
-                AttributeSet att = printService.getAttributes();
-                for (Attribute a : att.toArray()) {
-                    String attributeName;
-                    String attributeValue;attributeName = a.getName();
-                    attributeValue = att.get(a.getClass()).toString();
-                    System.out.println(attributeName +" : " + attributeValue);
-                    attribute += attributeName +" : " + attributeValue +"\n";
-                }
-                attribute += "----Printer END-----------------\n";
-         }
+        String attribute = "";
+        for (PrintService printService : printServices) {
+            String sPrinterName = printService.getName();
+            attribute += "--Printer Name---\n";
+            if (sPrinterName.equals("RETSOL RTP-80") || sPrinterName.equals("POS-58")) { //POS-58ew  
+                mPrinter = printService;
+                bFoundPrinter = true;
+            }
+
+            System.out.println("Printer Name::" + " : " + sPrinterName);
+            AttributeSet att = printService.getAttributes();
+            for (Attribute a : att.toArray()) {
+                String attributeName;
+                String attributeValue;
+                attributeName = a.getName();
+                attributeValue = att.get(a.getClass()).toString();
+                System.out.println(attributeName + " : " + attributeValue);
+                attribute += attributeName + " : " + attributeValue + "\n";
+            }
+            attribute += "----Printer END-----------------\n";
+        }
 //         showDataV2(attribute);
-          PrintService service1 = PrintServiceLookup.lookupDefaultPrintService();
-          //System.out.println("Default Printer::" +" : " + service1.getName());
-        if(bFoundPrinter){
+        PrintService service1 = PrintServiceLookup.lookupDefaultPrintService();
+        //System.out.println("Default Printer::" +" : " + service1.getName());
+        if (bFoundPrinter) {
             System.out.println("Printer POS-58 found");
-        }else{
+        } else {
             System.out.println("Printer POS-58 Not found");
         }
-        
-         try {
-        // Open the image file
+
+        try {
+            // Open the image file
             String testData = dataToPrint.toString();
             String modify_space = testData.replaceAll("%20", " ");
             String modify_newLine = modify_space.replaceAll("0X1", "\n");
             InputStream is = new ByteArrayInputStream(modify_newLine.getBytes());
-            DocFlavor flavor =  DocFlavor.INPUT_STREAM.AUTOSENSE   ;
+            DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
 
             // Find the default service
 //            PrintService service = PrintServiceLookup.lookupDefaultPrintService();
@@ -356,7 +362,7 @@ public class JavaProtocolApp {
 
             // Create the print job
             DocPrintJob job = service.createPrintJob();
-            Doc doc= new SimpleDoc(is, flavor, null);
+            Doc doc = new SimpleDoc(is, flavor, null);
 
             // Monitor print job events; for the implementation of PrintJobWatcher,
             PrintJobWatcher pjDone = new PrintJobWatcher(job);
@@ -367,68 +373,64 @@ public class JavaProtocolApp {
             pjDone.waitForDone();
 
             // It is now safe to close the input stream
-            is.close(); 
-         } catch (PrintException e) {
+            is.close();
+        } catch (PrintException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     //convert response in json object
-    private static String extractData(String data){
-       try{
-       JSONObject receviedObject = new JSONObject(data);
-       String toPrintData = receviedObject.getString("data");
-       return toPrintData.toString();
-       }catch(Exception exp){
-           return "Error";
-       }
-       
-        
+    private static String extractData(String data) {
+        try {
+            JSONObject receviedObject = new JSONObject(data);
+            String toPrintData = receviedObject.getString("data");
+            return toPrintData.toString();
+        } catch (Exception exp) {
+            return "Error";
+        }
+
     }
-    
-    public static  void showData(String data){
-        JFrame f=new JFrame("Protocol Content");  
-        JButton b=new JButton(data);  
-        System.out.println("Data="+data);
-        b.setBounds(0,0,500,500);  
-        
-        f.add(b);  
-        f.setSize(600,600);  
-        f.setLayout(null);  
+
+    public static void showData(String data) {
+        JFrame f = new JFrame("Protocol Content");
+        JButton b = new JButton(data);
+        System.out.println("Data=" + data);
+        b.setBounds(0, 0, 500, 500);
+
+        f.add(b);
+        f.setSize(600, 600);
+        f.setLayout(null);
         f.setVisible(true);
     }
-    
-    
-    public static String cleanData(String rawData){
+
+    public static String cleanData(String rawData) {
         String modified = rawData.replace("{\"data\":\"", "");
         String finalString = modified.replace("\"}", "");
         return finalString;
     }
-    
-    public static void showDataV2(String data){
+
+    public static void showDataV2(String data) {
         // Create and set up the window.  
-        System.out.println("Data="+data);
-        final JFrame frame = new JFrame("Scroll Pane Example");  
-  
+        System.out.println("Data=" + data);
+        final JFrame frame = new JFrame("Scroll Pane Example");
+
         // Display the window.  
-         
 //        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-  
         // set flow layout for the frame  
-        frame.getContentPane().setLayout(new FlowLayout());  
-  
-        JTextArea textArea = new JTextArea(20,20);  
-        textArea.append("data="+data);
-        JScrollPane scrollableTextArea = new JScrollPane(textArea);  
-  
-        scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
-        scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  
-  
-        frame.setSize(500, 500);  
-        frame.setVisible(true); 
-        frame.getContentPane().add(scrollableTextArea);  
+        frame.getContentPane().setLayout(new FlowLayout());
+
+        JTextArea textArea = new JTextArea(20, 20);
+        textArea.append("data=" + data);
+        JScrollPane scrollableTextArea = new JScrollPane(textArea);
+
+        scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.getContentPane().add(scrollableTextArea);
     }
-    
+
 }
